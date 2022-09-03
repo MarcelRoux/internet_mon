26eff0c449d334330ad416872f60cd3a875548a0
Marcel <marcel.roux.email@gmail.com>
2022-09-03 08:57:37 +0200

    Added parsing code for result.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from internet_mon.speedtest_interface import speedtest
+from internet_mon.utils import flatten_json
 
 
 def main():
@@ -7,6 +8,11 @@ def main():
     print('Running speedtest.')
     r = speedtest(status_updates=True)
     print(r)
+
+    r_flat = flatten_json(r)
+    print(f'r_flat: {r_flat}')
+    print(sorted(r_flat))
+    [print(f'{e}: {r_flat[e]}') for e in sorted(r_flat)]
 
 
 if __name__ == '__main__':
