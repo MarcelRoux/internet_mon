9c30247975494a5e32e1a1f19b424a21c2281a1e
Marcel <marcel.roux.email@gmail.com>
2022-09-03 08:44:07 +0200

    Updated speedtest call in main.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from internet_mon.speedtest_interface import *
+from internet_mon.speedtest_interface import speedtest
 
 
 def main():
     print('Hello, World!')
+
+    print('Running speedtest.')
+    r = speedtest(status_updates=True)
+    print(r)
 
 
 if __name__ == '__main__':
