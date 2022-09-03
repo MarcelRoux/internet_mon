9f776ab22c0e06433466bb78c8c0e578a1a8c022
Marcel <marcel.roux.email@gmail.com>
2022-09-03 17:15:16 +0200

    Completed test and log process.

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from internet_mon.speedtest_interface import speedtest
 from internet_mon.utils import flatten_json
+from internet_mon.db import insert_data
+
+
+DB_NAME_BASE = 'data/log.db'
 
 
 def main():
-    print('Hello, World!')
-
     print('Running speedtest.')
     r = speedtest(status_updates=True)
     print(r)
 
     r_flat = flatten_json(r)
-    print(f'r_flat: {r_flat}')
-    print(sorted(r_flat))
-    [print(f'{e}: {r_flat[e]}') for e in sorted(r_flat)]
+    # print(f'r_flat: {r_flat}')
+    # print(sorted(r_flat))
+    # [print(f'{e}: {r_flat[e]}') for e in sorted(r_flat)]
+
+    print('Logging data.')
+    insert_data(DB_NAME_BASE, r_flat)
+
+    print('Done.')
 
 
 if __name__ == '__main__':
