8ad2295904e93443dc462becbaf24494f1a6fdaa
Marcel <marcel.roux.email@gmail.com>
2022-09-04 20:04:53 +0200

    Implemented retry code.

--- internet_mon/speedtest_interface.py
+++ internet_mon/speedtest_interface.py
@@ -5,29 +5,39 @@ servers = []
 threads = None
 
 
-def speedtest(status_updates=False):
+def speedtest(status_updates=False, retries=3):
 
-    if (status_updates):
-        print('Configuring test.')
+    results_dict = None
 
-    s = ookla_speedtest.Speedtest()
+    for attempt in range(retries):
+        try:
+            if (status_updates):
+                print('Configuring test.')
 
-    if (status_updates):
-        print('Testing for best server.')
-    s.get_servers(servers)
-    s.get_best_server()
+            s = ookla_speedtest.Speedtest()
 
-    if (status_updates):
-        print('Download test.')
-    s.download(threads=threads)
+            if (status_updates):
+                print('Testing for best server.')
+            s.get_servers(servers)
+            s.get_best_server()
 
-    if (status_updates):
-        print('Upload test.')
-    s.upload(threads=threads)
+            if (status_updates):
+                print('Download test.')
+            s.download(threads=threads)
 
-    if (status_updates):
-        print('Done.')
-    results_dict = s.results.dict()
+            if (status_updates):
+                print('Upload test.')
+            s.upload(threads=threads)
 
-    # print(results_dict)
+            if (status_updates):
+                print('Done.')
+            results_dict = s.results.dict()
+
+        except:
+            # perhaps reconnect, etc.
+            continue
+
+        else:
+            break
+
     return results_dict
