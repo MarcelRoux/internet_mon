b4614343690a9aabdf44d0606bf3a28338c4f05d
Marcel <marcel.roux.email@gmail.com>
2022-09-04 20:12:46 +0200

    Updated environment variables.

--- internet_mon/config.py
+++ internet_mon/config.py
@@ -4,5 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_NAME = os.getenv('DB_NAME_BASE')
+DB_NAME = os.getenv('DB_NAME')
 SQL_CREATE_SCRIPT = os.getenv('SQL_CREATE_SCRIPT')
+SPEEDTEST_UPDATES = os.getenv('SPEEDTEST_UPDATES').lower() == 'true'
